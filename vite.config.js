e0f24dd3f7a92211c3c61051270704613eb66a26
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-05-19 23:55:23 +0200

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/annotation-project/',
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/annotation-project/',
 })
